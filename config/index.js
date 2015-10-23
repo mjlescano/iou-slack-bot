21de12b3d4c61967fa80d73d2f6e0c261950f880
Matías Lescano <matiasj.lescano@gmail.com>
2015-10-23 17:48:33 -0300

    add log bot

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
   mongoUrl: process.env.MONGOLAB_URI || 'mongodb://localhost/iou-slack-bot',
+  slackToken: process.env.SLACK_TOKEN || throw new Error('SLACK_TOKEN must be defined.')
 }
