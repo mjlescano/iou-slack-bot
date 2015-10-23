4770a00f3dc21c1f307791d148b79f55e70341b3
Matías Lescano <matiasj.lescano@gmail.com>
2015-10-23 17:56:03 -0300

    add slack token

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
   mongoUrl: process.env.MONGOLAB_URI || 'mongodb://localhost/iou-slack-bot',
-  slackToken: process.env.SLACK_TOKEN || throw new Error('SLACK_TOKEN must be defined.')
+  token: process.env.TOKEN || null
 }
