21832c77d25ae87b31aac7255f33b98ece39eab6
Matías Lescano <matiasj.lescano@gmail.com>
2016-07-10 11:19:22 -0300

    add MONGO_URL env config

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  mongoUrl: process.env.MONGOLAB_URI || 'mongodb://localhost/iou-slack-bot',
+  mongoUrl: process.env.MONGOLAB_URI || process.env.MONGO_URL || 'mongodb://localhost/iou-slack-bot',
   token: process.env.TOKEN || null
 }
